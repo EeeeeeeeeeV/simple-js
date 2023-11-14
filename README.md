ce64da009553bc79bacefe61bf13c214f00cb5e8
EeeeeeeeeeV <564569208@qq.com>
2023-11-14 21:11:14 +0800

    initialize

---
+++ README.md
@@ -0,0 +1,3 @@
+# React App脚手架(JavaScript)
+## 环境
+node 18.0.0
---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <div>React Simple App Using JS</div>,
+    document.getElementById('root')
+)
